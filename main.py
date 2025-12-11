e84fa8f202857f42f118eaf9f3d38e38538ff93d
ariel616-coc <ak.ariel616@gmail.com>
2025-12-11 23:04:39 +0200

    add more foncktsiot

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+import random
+
 class Coin:
-    def __init__(self, x, y):
+    def __init__(self, x, y, value = 10):
         self.center_x = x
         self.center_y = y
-        self.value = 10
+        self.value = value
 
 
 class Character:
-    def __init__(self, started_x, started_y, speed):
+    def __init__(self, started_x, started_y, speed, change_x = 0, change_y = 0):
         self.center_x = started_x
         self.center_y =started_y
         self.speed = speed
-        self.change_x = 0
-        self.change_y = 0
+        self.change_x = change_x
+        self.change_y = change_y
 
 
 class Player(Character):
@@ -21,9 +23,23 @@ class Player(Character):
         self.lives = 3
 
     def move(self):
-        self.center_x = self.change_x * self.speed
-        self.center_y = self.change_y * self.speed
+        self.center_x += self.change_x * self.speed
+        self.center_y += self.change_y * self.speed
 
 class Enemy(Character):
     def __init__(self, started_x, started_y, speed):
         super().__init__(started_x, started_y, speed)
+        self.time_to_change_direction = 0
+
+    def pick_new_direction(self):
+        movements = [(0, 1), (0, -1), (1, 0), (-1, 0), (0, 0)]
+        self.change_x = random.choice(movements)
+        self.change_y = random.choice(movements)
+        self.time_to_change_direction = random.uniform(0.3, 1.0)
+
+    def update(self,  delta_time = 1/60):
+
+
+
+class Wall:
+    def __init__(self):
