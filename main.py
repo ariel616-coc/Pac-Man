3c400948f3dd826b6f367e34ef9ab54a5f7900a6
ariel616-coc <ak.ariel616@gmail.com>
2026-02-11 18:19:15 +0200

    Update main.py
    3/5 update

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import random
+import arcade
 
-class Coin:
+
+TILE_SIZE = 32
+
+class Coin(arcade.Sprite):
     def __init__(self, x, y, value = 10):
+        super().__init__()
+        radius = TILE_SIZE // 8
+        texture = arcade.make_circle_texture(radius * 2, arcade.color.GOLDEN_YELLOW)
+        self.texture = texture
+        self.width = texture.width
+        self.height = texture.height
         self.center_x = x
         self.center_y = y
         self.value = value
 
 
-class Character:
-    def __init__(self, started_x, started_y, speed = 0, change_x = 0, change_y = 0):
+class Character (arcade.Sprite):
+    def __init__(self, started_x, started_y, speed = 0, change_x = 0, change_y = 0, color = arcade.color.YELLOW):
+        super().__init__()
+        radius = TILE_SIZE // 2 - 2
+        texture = arcade.make_circle_texture(radius * 2, color)
+        self.texture = texture
         self.center_x = started_x
         self.center_y = started_y
         self.speed = speed
+        self.width = texture.width
+        self.height = texture.height
         self.change_x = change_x
         self.change_y = change_y
 
@@ -26,9 +42,11 @@ class Player(Character):
         self.center_x += self.change_x * self.speed
         self.center_y += self.change_y * self.speed
 
+
 class Enemy(Character):
-    def __init__(self, started_x, started_y):
-        super().__init__(started_x, started_y)
+    def __init__(self, started_x, started_y, color = arcade.color.RED):
+        super().__init__(started_x, started_y, color)
+
         self.time_to_change_direction = 0
 
     def pick_new_direction(self):
@@ -38,7 +56,7 @@ class Enemy(Character):
         self.change_y = fate[1]
         self.time_to_change_direction = random.uniform(0.3, 1.0)
 
-    def update(self,  delta_time = 1/60):
+    def update(self, delta_time = 1/60):
         if self.time_to_change_direction == 0:
             self.pick_new_direction()
         self.center_x += self.change_x * self.speed
@@ -46,10 +64,126 @@ class Enemy(Character):
         self.time_to_change_direction -= delta_time
 
 
-class Wall:
+class Wall(arcade.Sprite):
     def __init__(self, center_x, center_y):
+        super().__init__()
+        radius = TILE_SIZE
+        texture = arcade.make_soft_square_texture(center_x,center_y , radius , arcade.color.BLUE)
+        self.width = texture.width
+        self.height = texture.height
+        self.texture = texture
         self.center_x = center_x
         self.center_y = center_y
+
+
+class PacmanGame:
+    def __init__(self):
+        self.wall_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+        self.ghost_list = arcade.SpriteList()
+        self.player_list = arcade.SpriteList()
+        self.player = None
+        self.game_over = False
+        self.background_color = arcade.color.BLACK
+        self.start_x = 0
+        self.start_y = 0
+
+    def setup(self):
+        self.wall_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+        self.ghost_list = arcade.SpriteList()
+        self.player_list = arcade.SpriteList()
+        self.game_over = False
+        for row_idx, row in enumerate(LEVEL_MAP):
+            for col_idx, cell in enumerate(row):
+                x = col_idx * TILE_SIZE + TILE_SIZE / 2
+                y = (row - row_idx - 1) * TILE_SIZE + TILE_SIZE / 2
+                if LEVEL_MAP[x, y] is wall:
+                    self.wall_list.append((x, y))
+
+                elif LEVEL_MAP[x, y] is coin:
+                    self.coin_list.append((x, y))
+
+                elif LEVEL_MAP[x, y] is ghost:
+                    self.ghost_list.append((x, y))
+
+                elif LEVEL_MAP[x, y] is player:
+                    self.player_list.append((x, y))
+
+    def on_draw(self):
+        self.clear()
+
+        self.wall_list.draw()
+        self.coin_list.draw()
+        self.ghost_list.draw()
+        self.player_list.draw()
+
+        arcade.draw_text(
+            f"Score: {self.player.score}",
+            10, self.player.height - 30,
+            arcade.color.WHITE, 16
+        )
+
+        arcade.draw_text(
+            f"Lives: {self.player.lives}",
+            10, self.player.height - 55,
+            arcade.color.WHITE, 16
+        )
+
+        if self.game_over:
+            arcade.draw_text(
+                "GAME OVER",
+                self.player.width // 2,
+                self.player.height // 2,
+                arcade.color.RED,
+                40,
+                anchor_x="center",
+                anchor_y="center"
+            )
+
+    def on_key_press(self, key, modifiers):
+        """
+        מטפלת בלחיצות מקשים מהמקלדת.
+
+        המתודה מגיבה לקלט של המשתמש בהתאם למצב המשחק:
+        - במצב הפסד: לחיצה על מקש SPACE מאתחלת את המשחק מחדש.
+        - בזמן משחק פעיל: חיצי המקלדת משנים את כיוון התנועה של השחקן.
+
+        :param key: המקש שנלחץ (קבוע מתוך arcade.key)
+        :param modifiers: מקשי עזר שנלחצו יחד עם המקש (Shift, Ctrl וכו')
+        :return: None
+        """
+
+        # מצב הפסד – אתחול מחדש
+
+        if self.game_over:
+            # אם המשחק נגמר, רק SPACE פעיל לצורך התחלה מחדש
+            if key == arcade.key.SPACE:
+                self.setup()
+            return  # מונע המשך טיפול במקשים אחרים
+
+        # משחק פעיל – שליטה בשחקן
+
+        # חץ למעלה – תנועה למעלה (ציר Y חיובי)
+        if key == arcade.key.UP:
+            self.player.change_y = 1
+            self.player.change_x = 0  # מבטיח תנועה בציר אחד בלבד
+
+        # חץ למטה – תנועה למטה (ציר Y שלילי)
+        elif key == arcade.key.DOWN:
+            self.player.change_y = -1
+            self.player.change_x = 0
+
+        # חץ שמאלה – תנועה שמאלה (ציר X שלילי)
+        elif key == arcade.key.LEFT:
+            self.player.change_x = -1
+            self.player.change_y = 0
+
+        # חץ ימינה – תנועה ימינה (ציר X חיובי)
+        elif key == arcade.key.RIGHT: 
+            self.player.change_x = 1
+            self.player.change_y = 0
+
 
 
 
